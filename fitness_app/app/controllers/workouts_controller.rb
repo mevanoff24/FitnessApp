6e0069993adce2dde2a7d87ebcf38b63f5c0f9b3
Matt Evanoff <mevanoff24@gmail.com>
2015-03-31 13:14:05 -0700

    update work

--- fitness_app/app/controllers/workouts_controller.rb
+++ fitness_app/app/controllers/workouts_controller.rb
@@ -11,13 +11,11 @@ class WorkoutsController < ApplicationController
 	end
 	
 	def new
-		@user_workout = User.find_by(uid: params[:uid]).workouts.new
 		@user = User.find_by(uid: params[:uid])
 		@workout = Workout.all
 	end
 
 	def create
-		p params
 		@user_workout = User.find_by(uid: params[:uid]).workouts.new(title: params[:workouts][:title], description: params[:workouts][:description])
 		@user_workout.save!
 		redirect_to articles_path
@@ -30,7 +28,7 @@ class WorkoutsController < ApplicationController
 	end
 
 	def update
-		@user_workout = User.find_by(uid: params[:uid]).workouts.update(title: params[:workouts][:title], description: params[:workouts][:description])
+		User.find_by(uid: params[:uid]).workouts.update(title: params[:workouts][:title], description: params[:workouts][:description])
 		redirect_to articles_path
 	end
 end
