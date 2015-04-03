f2b46e296c257f90d22d5df77d7a4216b9e80bde
Matt Evanoff <mevanoff24@gmail.com>
2015-04-03 09:43:32 -0700

    addded validation and destroy action

--- fitness_app/app/models/workout.rb
+++ fitness_app/app/models/workout.rb
@@ -1,5 +1,6 @@
 class Workout < ActiveRecord::Base
-	has_many :routines
+	has_many :routines, dependant: :destroy 
 	belongs_to :user
 	accepts_nested_attributes_for :routines, allow_destroy: true
+	validates_presence_of :title
 end
